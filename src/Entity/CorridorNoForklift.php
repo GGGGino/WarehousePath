6ccf7010723ff46c1958c27b29b40b85ca49e367
David Ginanni <david.ginanni@gmail.com>
2019-02-17 03:43:04 +0100

    added some tests

---
+++ src/Entity/CorridorNoForklift.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace GGGGino\WarehousePath\Entity;
+
+/**
+ * No very used but will be useful or future development.
+ * Now Used for tosting
+ *
+ * Class CorridorNoForklift
+ * @package GGGGino\WarehousePath\Entity
+ */
+class CorridorNoForklift extends Place
+{
+    /**
+     * @inheritdoc
+     */
+    public function __construct($name = "")
+    {
+        $this->originalWeight = 5;
+
+        parent::__construct($name);
+    }
+
+    /**
+     * Describe if the place will be walkable or not
+     *
+     * @return boolean
+     */
+    public function isWalkable()
+    {
+        return true;
+    }
+}
---
+++ tests/PlaceCollectorTest.php
@@ -0,0 +1,46 @@
+<?php
+
+use GGGGino\WarehousePath\Entity\Corridor;
+use GGGGino\WarehousePath\Entity\CorridorNoForklift;
+use GGGGino\WarehousePath\Entity\Location;
+use GGGGino\WarehousePath\Entity\Place;
+use GGGGino\WarehousePath\Entity\Wall;
+use GGGGino\WarehousePath\PlacesCollector;
+use GGGGino\WarehousePath\Warehouse;
+use GGGGino\WarehousePath\WarehouseTree;
+use PHPUnit\Framework\TestCase;
+
+final class PlaceCollectorTest extends TestCase
+{
+    public function testGetTotalPlaces(): void
+    {
+        /** @var PlacesCollector $placeCollector */
+        $placeCollector = new PlacesCollector();
+
+        $placeCollector->addBasePlaceTypes();
+
+        $this->assertEquals(3, count($placeCollector->getPlacesType()));
+    }
+
+    public function testGetByWeight(): void
+    {
+        /** @var PlacesCollector $placeCollector */
+        $placeCollector = new PlacesCollector();
+
+        $placeCollector->addBasePlaceTypes();
+
+        $this->assertInstanceOf(Corridor::class, $placeCollector->getPlaceTypeByWeight(2));
+    }
+
+    public function testAddAndGetByWeight(): void
+    {
+        /** @var PlacesCollector $placeCollector */
+        $placeCollector = new PlacesCollector();
+
+        $placeCollector->addBasePlaceTypes();
+
+        $placeCollector->addPlaceType(new CorridorNoForklift());
+
+        $this->assertInstanceOf(CorridorNoForklift::class, $placeCollector->getPlaceTypeByWeight(5));
+    }
+}
